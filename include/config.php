e4fc83ee33f02d4fe8c279b7c75c54d8dad14728
Pedro Arrieta <pedroarrieta25@hotmail.com>
2018-09-10 17:01:16 -0500

    subida a la nuve

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
 	$DB_Server = "localhost";
-	$DB_Username ="root";
-	$DB_Password = "";
-	$DB_DBName = "po";
+	$DB_Username ="tayron17";
+	$DB_Password = "178718397406";
+	$DB_DBName = "planetonline";
 	date_default_timezone_set("America/Panama");
 ///////////////////////////////////////////////////////////////////////////////////////
 ?>
